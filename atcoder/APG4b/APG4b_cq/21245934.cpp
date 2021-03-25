691d463e01e9bd864e926bb34cdc63cc754e78df
aozorarb <melon0802yota800@icloud.com>
2021-03-25 21:13:55 +0900

    APG4b_cq

---
+++ atcoder/APG4b/APG4b_cq/21245934.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  string op;
+  cin >> A >> op >> B;
+  if (op == "+") {
+    cout << A + B << endl;
+  }else if (op == "-"){
+    cout << A - B << endl;
+  }else if (op == "*"){
+    cout << A * B << endl;
+  }else if (op == "/" && B != 0){
+     int quotient = A / B;
+     cout << quotient << endl;
+  }else{
+  cout << "error" << endl;
+  }
+  // ここにプログラムを追記
+  
+}
+
+
+
