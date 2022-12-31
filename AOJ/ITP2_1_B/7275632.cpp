2150f0e14a22981445246ee6ac46ec81d60ce176
aozorarb <melon0802yota800@icloud.com>
2022-12-31 23:14:13 +0900

    ITP2_1_B

---
+++ AOJ/ITP2_1_B/7275632.cpp
@@ -0,0 +1,26 @@
+#include <deque>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int q, v, d, o;
+    deque<int> A;
+    cin >> q;
+    for(int i=0;i<q;++i) {
+        int o, d, v;
+        cin >> o;
+        if(o == 0) {
+            cin >> d >> v;
+            if(d) A.push_back(v);
+            else A.push_front(v);
+        } else if(o == 1) {
+            cin >> v;
+            cout << A[v] << endl;
+        } else if(o == 2) {
+            cin >> d;
+            if(d) A.pop_back();
+            else A.pop_front();
+        }
+    }
+}
+
