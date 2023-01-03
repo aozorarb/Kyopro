9ea65a930353726ec321a4dd674a58592496e643
aozorarb <melon0802yota800@icloud.com>
2023-01-03 19:00:06 +0900

    ITP2_2_B

---
+++ AOJ/ITP2_2_B/7284517.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<queue<int> > Q(n);
+    for(int i=0;i<q;++i) {
+        int o, t, x;
+        cin >> o >> t;
+        if(o == 0) {
+            cin >> x;
+            Q[t].push(x);
+        } else if(o == 1) {
+            if(!Q[t].empty()) cout << Q[t].front() << endl;
+        } else if(o == 2) {
+            if(!Q[t].empty()) Q[t].pop();
+        }
+    }
+}
+            
+
+
+
