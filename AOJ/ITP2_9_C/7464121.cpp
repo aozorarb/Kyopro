8d8f25527c7a3e66f81b22d9b0dd066cf79272e8
aozorarb <melon0802yota800@icloud.com>
2023-02-12 09:49:32 +0900

    ITP2_9_C

---
+++ AOJ/ITP2_9_C/7464121.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n;
+  set<int> A, B, C;
+  int tmp;
+  for(int i=0;i<n;++i) {
+    cin >> tmp;
+    A.insert(tmp);
+  }
+  cin >> m;
+  for(int i=0;i<m;++i){
+    cin >> tmp;
+    B.insert(tmp);
+  }
+  set_difference(A.begin(), A.end(), B.begin(), B.end(), inserter(C, C.end()));
+  for(set<int>::iterator it=C.begin();it!=C.end();++it) {
+    cout << *it << endl;
+  }
+}
+
