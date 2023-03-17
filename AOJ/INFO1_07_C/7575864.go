320ec48bf61a4864ed8c26035b0bbaa2a4513827
aozorarb <melon0802yota800@icloud.com>
2023-03-17 14:46:50 +0900

    INFO1_07_C

---
+++ AOJ/INFO1_07_C/7575864.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+    var a int
+    fmt.Scan(&a)
+    if a > 0 {
+        fmt.Println(1)
+    } else if a < 0 {
+        fmt.Println(-1)
+    } else {
+        fmt.Println(0)
+    }
+}
