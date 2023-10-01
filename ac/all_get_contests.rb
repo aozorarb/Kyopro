92c5d121de39ff05ae6db7f84775118a0f823980
aozorarb <melon0802yota800@icloud.com>
2023-10-01 17:54:11 +0900

    add all get abc/arc/agc script

---
+++ ac/all_get_contests.rb
@@ -0,0 +1,46 @@
+Ignore_contest = %w(abc316)
+Contests_min = 1
+Contests = %w(abc arc agc)
+def search_missing_files
+  ret = {}
+  ret["missing_files"] = {}
+  ret['max'] = {}
+  Contests.each do |cont|
+    list = Dir.glob("#{cont}*").map { |f| f.slice(/\d+/).to_i }
+    ret['max'][cont] = list.max
+    next if list.empty?
+    Contests_min.upto list.max do |i|
+       ret["missing_files"][cont] = [] unless ret["missing_files"].key?(cont)
+       ret["missing_files"][cont] << "#{format('%03d', i)}" unless list.include? i
+    end
+  end
+  ret
+end
+
+def download_all(info)
+  Contests.each do |cont|
+    info['missing_files'][cont].each do |miss|
+      `at_coder_friends setup #{cont}#{miss}` unless Ignore_contest.include? "#{cont}#{miss}"
+      unless $? == 0
+        puts "maybe not found #{cont}#{miss}"
+      else
+        puts "get #{cont}#{miss}"
+      end
+    end
+
+    (info['max'][cont] .. nil).each do |num|
+      `at_coder_friends setup #{cont}#{num} 2>> /dev/null`
+      unless $? == 0
+        puts "Got all #{cont}"
+        break
+      end
+      puts "get #{cont}#{num}"
+    end
+  end
+end
+
+def main
+  info = search_missing_files
+  download_all(info)
+end
+main
